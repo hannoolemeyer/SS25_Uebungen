b72a32e362eb0f86e4d40182cec79af3d5bc2c2a
hannoolemeyer <43036022+hannoolemeyer@users.noreply.github.com>
2025-05-08 13:51:13 +0200

    Update Autovermietung.cpp

--- Autovermietung.cpp
+++ Autovermietung.cpp
@@ -14,9 +14,6 @@ Autovermietung::~Autovermietung() {
 
 
 void Autovermietung::dialog() {
-    Mietwagen* fahrzeug = new Mietwagen();  // dynamisch erzeugt
-    fahrzeuge.push_back(fahrzeug);          // Zeiger speichern
-
     int auswahl;
 
     do {
@@ -25,40 +22,133 @@ void Autovermietung::dialog() {
         cout << "2: Fahrt anzeigen" << endl;
         cout << "3: Alle Fahrten anzeigen" << endl;
         cout << "4: Fahrt löschen" << endl;
+        cout << "5: Fahrzeug hinzufügen" << endl;
+        cout << "6: Alle Fahrzeuge ausgeben" << endl;
         cout << "0: Programm beenden" << endl;
         cout << "Auswahl: ";
         cin >> auswahl;
 
         switch (auswahl) {
-            case 1:
-                (*fahrzeug).anmieten();  // statt fahrzeug->anmieten();
+            case 1: {
+                Mietwagen* mw = mietwagenSuchen();
+                if (mw != nullptr) {
+                    mw->fahrzeugAnzeigen();
+                    mw->anmieten();
+                }
                 break;
+            }
 
             case 2: {
-                int nummer;
-                cout << "Fahrtnummer eingeben: ";
-                cin >> nummer;
-                (*fahrzeug).fahrtAnzeigen(nummer);
+                Mietwagen* mw = mietwagenSuchen();
+                if (mw != nullptr) {
+                    mw->fahrzeugAnzeigen();
+                    int nummer;
+                    cout << "Fahrtnummer eingeben: ";
+                    cin >> nummer;
+                    mw->fahrtAnzeigen(nummer);
+                }
                 break;
             }
 
-            case 3:
-                (*fahrzeug).alleFahrtenAnzeigen();
+            case 3: {
+                Mietwagen* mw = mietwagenSuchen();
+                if (mw != nullptr) {
+                    (*mw).fahrzeugAnzeigen();
+
+                    int sortWahl;
+                    cout << "Sortieren nach Name (1 = aufsteigend, 2 = absteigend)? ";
+                    cin >> sortWahl;
+                    bool aufsteigend = (sortWahl == 1);
+
+                    (*mw).sortiereFahrten(aufsteigend);
+                    (*mw).alleFahrtenAnzeigen();
+                }
                 break;
+            }
 
             case 4: {
-                int nummer;
-                cout << "Buchungsnummer der zu löschenden Fahrt: ";
-                cin >> nummer;
-                (*fahrzeug).fahrtLoeschen(nummer);
+                Mietwagen* mw = mietwagenSuchen();
+                if (mw != nullptr) {
+                    mw->fahrzeugAnzeigen();
+                    int nummer;
+                    cout << "Buchungsnummer der zu löschenden Fahrt: ";
+                    cin >> nummer;
+                    mw->fahrtLoeschen(nummer);
+                }
                 break;
             }
+
+            case 5: {
+                Mietwagen* neuesFahrzeug = new Mietwagen();
+                fahrzeuge.push_back(neuesFahrzeug);
+                cout << "Neues Fahrzeug wurde hinzugefügt." << endl;
+                break;
+            }
+            case 6: {
+                if (fahrzeuge.empty()) {
+                    cout << "Keine Fahrzeuge vorhanden." << endl;
+                } else {
+                    int sortWahl;
+                    cout << "Sortieren nach Marke (1 = aufsteigend, 2 = absteigend)? ";
+                    cin >> sortWahl;
+                    bool aufsteigend = (sortWahl == 1);
+
+                    sortiereFahrzeuge(aufsteigend);
+
+                    for (Mietwagen* fahrzeug : fahrzeuge) {
+                        (*fahrzeug).fahrzeugAnzeigen();
+                        (*fahrzeug).alleFahrtenAnzeigen();
+                        cout << "-----------------------------" << endl;
+                    }
+                }
+                break;
+            }
+
             case 0:
                 cout << "Programm wird beendet." << endl;
                 break;
 
             default:
-                cout << "Ungültige Eingabe. Bitte 0–3 wählen." << endl;
+                cout << "Ungültige Eingabe. Bitte 0–5 wählen." << endl;
         }
     } while (auswahl != 0);
 }
+
+Mietwagen* Autovermietung::mietwagenSuchen() {
+    if (fahrzeuge.empty()) {
+        cout << "Keine Fahrzeuge vorhanden!" << endl;
+        return nullptr;
+    }
+
+    cout << "\nVerfügbare Fahrzeuge:" << endl;
+    for (size_t i = 0; i < fahrzeuge.size(); ++i) {
+        cout << i << ": ";
+        fahrzeuge[i]->fahrzeugAnzeigen();
+    }
+
+    cout << "Bitte Fahrzeugindex wählen: ";
+    int index;
+    cin >> index;
+
+    if (index < 0 || index >= static_cast<int>(fahrzeuge.size())) {
+        cout << "Ungültiger Index!" << endl;
+        return nullptr;
+    }
+
+    return fahrzeuge[index];
+}
+#include <algorithm>  // ganz oben
+
+void Autovermietung::sortiereFahrzeuge(bool aufsteigend) {
+    std::sort(fahrzeuge.begin(), fahrzeuge.end(),
+        [aufsteigend](Mietwagen* erster, Mietwagen* zweiter) {
+            return aufsteigend
+                ? (*erster).getMarke() < (*zweiter).getMarke()
+                : (*erster).getMarke() > (*zweiter).getMarke();
+        });
+}
+void Autovermietung::addFahrzeug(Mietwagen* wagen) {
+    fahrzeuge.push_back(wagen);
+}
+
+
